d952a7bd3fe5e2e99ab1818e24c3e5fce65b322b
BKrone <kr1.brandon@gmail.com>
2020-06-30 07:28:48 -0500

    Adding StatePropertyFactory

--- lib/state/factory.py
+++ lib/state/factory.py
@@ -6,3 +6,9 @@ class StateHandlerFactory(FactoryType):
     @classmethod
     def create(cls, *args, **kwargs):
         return StateHandler(*args, **kwargs)
+
+
+class StatePropertyFactory(FactoryType):
+    @classmethod
+    def create(cls, *args, **kwargs):
+        return StateProperty(*args, **kwargs)
