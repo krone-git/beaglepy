ff6f483a5b3028f1d7a141db6e3af4339ab86df1
BKrone <kr1.brandon@gmail.com>
2020-06-30 07:31:56 -0500

    Addin 'suite' kwarg to Session __init__

--- lib/session.py
+++ lib/session.py
@@ -2,8 +2,9 @@ from .engine import EngineFactory
 
 
 class Session:
-    def __init__(self):
-        self._engine = EngineFactory.create()
+    def __init__(self, *args, **kwargs):
+        self._engine = EngineFactory.create(*args, **kwargs)
+        self._export_handler = ExportHandlerFactory(*args, **kwargs) #...
 
     @property
     def history(self):
@@ -13,5 +14,17 @@ class Session:
     def state(self):
         return self._engine.state
 
+    @property
+    def engine(self):
+        return self._engine
+
+    @property
+    def export_handler(self):
+        return self._export_handler
+
     def execute(self):
         self._engine.execute()
+
+    def export(self, location):
+        #...
+        pass
